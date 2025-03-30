09ded7617ca66c1a2221a3e95e8c20f2e804b43e
Yann Stephant <stpht@users.noreply.github.com>
2025-03-31 00:23:23 +0200

    Corrected import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { Routes, Route } from 'react-router-dom'; 
 import Login from './components/Login';
 import Register from './components/Register';
 import Header from './components/Header';
