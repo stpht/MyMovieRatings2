1308c9d0d8ca5a529d710cfdfe5deb0540a6ef02
Yann Stephant <stpht@users.noreply.github.com>
2025-03-30 23:42:54 +0200

    updated /app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from './components/Login';
 import Header from './components/Header';
 import WatchedMovieList from './components/WatchedMovieList';
 import SearchBar from './components/SearchBar';
+import SearchResults from './components/SearchResults'; 
 
 function App() {
   return (
