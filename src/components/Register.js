a494a4ec998714e688dc54d9c5d6a1df9353adf3
Yann Stephant <stpht@users.noreply.github.com>
2025-03-30 23:49:36 +0200

    updated /login.js /register.js

---
+++ src/components/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Register() {
+  return (
+    <div>
+      <h2>Register</h2>
+      {/* Registration form will go here */}
+    </div>
+  );
+}
+
+export default Register;
