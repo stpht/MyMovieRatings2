557b22aa81dbd0ca4823a7b7677768af62d1e934
Yann Stephant <stpht@users.noreply.github.com>
2025-03-30 23:17:58 +0200

    created /Header.js /SearchBar.js updated /App.js added basic styling /App.css

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+  function Header() {
+    return (
+      <header>
+        <h1>MyMovieRatings2</h1>
+      </header>
+    );
+  }
+
+  export default Header;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+  function SearchBar() {
+    return (
+      <input 
+        type="text" 
+        placeholder="Search for a movie..." 
+      />
+    );
+  }
+
+  export default SearchBar;
