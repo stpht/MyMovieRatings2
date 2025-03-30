b99a2be1bd114412cab203023f3db674a6b74abb
Yann Stephant <stpht@users.noreply.github.com>
2025-03-31 00:11:24 +0200

    replaced the direct rendering of components with <Route> components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import Login from './components/Login';
+import Register from './components/Register';
 import Header from './components/Header';
 import WatchedMovieList from './components/WatchedMovieList';
 import SearchBar from './components/SearchBar';
-import SearchResults from './components/SearchResults'; 
+import SearchResults from './components/SearchResults';
 
 const AppContainer = styled.div`
   background: linear-gradient(to bottom, #222, #333);
@@ -19,12 +21,20 @@ const AppContainer = styled.div`
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <SearchBar />
-      <Login />
-      <WatchedMovieList />
-    </div>
+    <AppContainer>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/movies" element={
+          <>
+            <Header />
+            <SearchBar />
+            <SearchResults />
+            <WatchedMovieList />
+          </>
+        } />
+      </Routes>
+    </AppContainer>
   );
 }
 
